0a87f9126cb8d63f7e024466696c06d4b1784df6
Valentin Metz <Valentin.Metz@web.de>
2023-09-02 05:56:44 +0200

    CI/CD pipeline

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,14 @@
 
 //! Documentation and final report for the P2P DHT project.
 //!
-//! Provides an abstraction over a queue.  When the abstraction is used
-//! there are these advantages:
-//! - Fast
-//! - [`Easy`]
+//! Our implementation is split into two core modules:
+//! - [`api_communication`]
+//! - [`chord`]
 //!
-//! [`Easy`]: http://thatwaseasy.example.com
+//! The [`api_communication`] module
+//!
+//! The main executable of this program `p2p_dht` takes one command line argument, `-c <config>`,
+//! where `<config>` is the path to a configuration file.
+//! The configuration file is expected to be in the INI format. The following configuration options are available:
 mod api_communication;
 pub mod chord;
