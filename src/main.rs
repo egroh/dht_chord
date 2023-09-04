971d902660e82cd9c08d0ec4b45484e3d160bb72
Eddie Groh <eddie.groh@tum.de>
2023-09-04 22:49:10 +0200

    Implemented user friendly command line output for command line arguments and config arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(async_closure)]
 #![allow(dead_code)]
+
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 
@@ -7,11 +9,13 @@ use std::net::{SocketAddr, ToSocketAddrs};
 use std::time::Duration;
 
 use crate::chord::Chord;
-
+use anyhow::{anyhow, Result};
 use env_logger::Env;
-use ini::ini;
+use ini::macro_safe_load;
 use tokio::task::JoinHandle;
 use tokio_util::sync::CancellationToken;
+
+type IniMap = HashMap<String, HashMap<String, Option<String>>>;
 
 mod api_communication;
 mod chord;
@@ -108,63 +112,123 @@ impl P2pDht {
     }
 }
 
-async fn create_dht_from_command_line_arguments() -> P2pDht {
+fn load_duration(key1: &str, key2: &str, config: &IniMap) -> Result<u64> {
+    Ok(config
+        .get(key1)
+        .ok_or(anyhow!("Category {} not found", key1))?
+        .get(key2)
+        .ok_or(anyhow!("Key {} not found", key2))?
+        .clone()
+        .ok_or(anyhow!("Key not found in config"))?
+        .parse::<u64>()?)
+}
+
+fn load_address(key1: &str, key2: &str, config: &IniMap) -> Result<SocketAddr> {
+    config
+        .get(key1)
+        .ok_or(anyhow!("Category {} not found", key1))?
+        .get(key2)
+        .ok_or(anyhow!("Key {} not found", key2))?
+        .clone()
+        .ok_or(anyhow!("Key not found in config"))?
+        .to_socket_addrs()?
+        .next()
+        .ok_or(anyhow!("No socket address could be parsed"))
+}
+
+async fn create_dht_from_command_line_arguments() -> Option<P2pDht> {
     let args = env::args().collect::<Vec<String>>();
-    assert!(args.len() >= 2, "Usage: {} -c <config>", args[0]);
-    assert_eq!(args[1], "-c", "Usage: {} -c <config>", args[0]);
-    let config = ini!(&args[2]);
+
+    if args.len() == 2 {
+        println!("Error: Expected two arguments");
+        println!("Usage: {} -c <config>", args[0]);
+        return None;
+    }
+
+    if args[1] != "-c" {
+        println!("Error: Unsupported option {}", args[1]);
+        println!("Usage: {} -c <config>", args[0]);
+        return None;
+    }
+
+    let config = match macro_safe_load(&args[2]) {
+        Ok(conf) => conf,
+        Err(e) => {
+            println!("Error: Cannot load file {}", args[2]);
+            println!("{}", e);
+            return None;
+        }
+    };
+
     let default_store_duration = Duration::from_secs(
-        config["dht"]["default_store_duration"]
-            .clone()
-            .unwrap()
-            .parse()
-            .unwrap(),
-    );
-    let max_store_duration = Duration::from_secs(
-        config["dht"]["max_store_duration"]
-            .clone()
-            .unwrap()
-            .parse()
-            .unwrap(),
+        match load_duration("dht", "default_store_duration", &config) {
+            Ok(value) => value,
+            Err(e) => {
+                println!("Error: Cannot load \"default_store_duration\" from the config");
+                println!("{}", e);
+                return None;
+            }
+        },
     );
 
-    let p2p_address = config["dht"]["p2p_address"]
-        .clone()
-        .unwrap()
-        .to_socket_addrs()
-        .unwrap()
-        .next()
-        .unwrap();
-    let api_address = config["dht"]["api_address"]
-        .clone()
-        .unwrap()
-        .to_socket_addrs()
-        .unwrap()
-        .next()
-        .unwrap();
+    let max_store_duration =
+        Duration::from_secs(match load_duration("dht", "max_store_duration", &config) {
+            Ok(value) => value,
+            Err(e) => {
+                println!("Error: Cannot load \"max_store_duration\"  from the config");
+                println!("{}", e);
+                return None;
+            }
+        });
+
+    let p2p_address = match load_address("dht", "p2p_address", &config) {
+        Ok(value) => value,
+        Err(e) => {
+            println!("Error: Cannot load \"p2p_address\" from the config");
+            println!("{}", e);
+            return None;
+        }
+    };
+
+    let api_address = match load_address("dht", "api_address", &config) {
+        Ok(value) => value,
+        Err(e) => {
+            println!("Error: Cannot load \"api_address\" from the config");
+            println!("{}", e);
+            return None;
+        }
+    };
 
     let initial_peer = config["dht"]
         .get("bootstrap_node")
         .and_then(|address| address.clone().unwrap().to_socket_addrs().unwrap().next());
 
-    P2pDht::new(
-        default_store_duration,
-        max_store_duration,
-        p2p_address,
-        api_address,
-        initial_peer,
-        true,
-        true,
+    Some(
+        P2pDht::new(
+            default_store_duration,
+            max_store_duration,
+            p2p_address,
+            api_address,
+            initial_peer,
+            true,
+            true,
+        )
+        .await,
     )
-    .await
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let mut dht = create_dht_from_command_line_arguments().await;
-
-    dht.await_termination().await;
+    match create_dht_from_command_line_arguments().await {
+        None => {
+            // Nothing to do but terminate
+        }
+        Some(mut dht) => {
+            // DHT has been created and is running, we wait for its termination
+            dht.await_termination().await
+        }
+    }
 
     Ok(())
 }
